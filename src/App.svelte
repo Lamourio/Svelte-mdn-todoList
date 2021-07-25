610070fc1dbe2179c1ddb5107d05a6a23e02dc38
marwane <lmarwane0@gmail.com>
2021-07-25 12:41:09 +0100

    add store to todos

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,14 @@
 <script>
-import Alert from "./components/Alert.svelte";
-
-	import Todos from "./components/Todos.svelte";
-	let todos = [
-		{ id: 1, name: "Create a Svelte starter app", completed: true },
-    	{ id: 2, name: "Create your first component", completed: true },
-    	{ id: 3, name: "Complete the rest of the tutorial", completed: false },
-	]
+    import Alert from "./components/Alert.svelte";
+    import Todos from "./components/Todos.svelte";
+	import { todos } from './stores.js'
+    $todos = [
+        { id: 1, name: "Create a Svelte starter app", completed: true },
+        { id: 2, name: "Create your first component", completed: true },
+        { id: 3, name: "Complete the rest of the tutorial", completed: false },
+    ];
+    // $: console.log("todos", todos);
 </script>
-<Alert/>
-<Todos {todos}/>
+
+<Alert />
+<Todos bind:todos={$todos} />
--- src/stores.js
+++ src/stores.js
@@ -1,2 +1,3 @@
 import { writable } from "svelte/store";
 export const alert = writable('welcome to the To-Do list app!');
+export const todos = writable([]);
